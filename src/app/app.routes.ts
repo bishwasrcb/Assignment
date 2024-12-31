fbdc0de1881242caf27ed9572e208580909a6438
Bishwas Rcb <bishwas.rcb@gmail.com>
2024-12-31 18:14:09 +0530

    modified product component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from '../login/login.component';
 import { ProductsComponent } from '../products/products.component';
 import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
+import { ProductDetailsComponent } from '../product-details/product-details.component';
 
 export const routes: Routes = [
     {path:'', component: LoginComponent},
     {path:'login', component: LoginComponent},
     {path:'products', component: ProductsComponent},
+    {path:'product-details', component: ProductDetailsComponent},
     {path: '**', component: PageNotFoundComponent}
 ];
--- src/products/products.component.ts
+++ src/products/products.component.ts
@@ -5,15 +5,13 @@ import { MatCardModule } from '@angular/material/card';
 import { MatSidenavModule } from '@angular/material/sidenav';
 // IMPORT STAR RATING MODULE
 import { StarRatingModule } from 'angular-star-rating';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faRupee,faIndianRupee, faDownload, faStar, faStarHalf } from '@fortawesome/free-solid-svg-icons';
 import {
   FontAwesomeModule,
   FaIconLibrary,
 } from '@fortawesome/angular-fontawesome';
-import { faSquare, faCheckSquare } from '@fortawesome/free-solid-svg-icons';
 import {
-  faSquare as farSquare,
-  faCheckSquare as farCheckSquare,
+  faCheckSquare
 } from '@fortawesome/free-regular-svg-icons';
 import {
   faStackOverflow,
@@ -21,35 +19,28 @@ import {
   faMedium,
 } from '@fortawesome/free-brands-svg-icons';
 
+import { Routes, RouterModule } from '@angular/router';
+
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [MatSidenavModule, MatButtonModule, CommonModule, MatCardModule, StarRatingModule, FontAwesomeModule],
+  imports: [MatSidenavModule, MatButtonModule, CommonModule, RouterModule, MatCardModule, StarRatingModule, FontAwesomeModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent {
   showFiller = false;
-  icon = faCoffee;
-  // faCoffee = faCoffee;
-
-  stars: number[] = [1, 2, 3, 4, 5];
-  selectedValue: number = 0;
+  Rupee = faIndianRupee;
+  rating = faStar;
+  halfRating = faStarHalf;
 
   constructor(library: FaIconLibrary) {
     library.addIcons(
-      faSquare,
       faCheckSquare,
-      farSquare,
-      farCheckSquare,
       faStackOverflow,
       faGithub,
       faMedium
     );
   }
 
-  countStar(star: any) {
-    this.selectedValue = star;
-    console.log('Value of star', star);
-  }
 }
